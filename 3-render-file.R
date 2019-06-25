205d389a192072ab217a7b9fee3c977893d56434
wjamesTMC <jamesw@csps.com>
2019-06-25 19:57:48 -0400

    Revisions to handle arbitrary # of surverys across all code sets.

--- 3-render-file.R
+++ 3-render-file.R
@@ -1,6 +1,5 @@
 library(rmarkdown)render("2-CustSatReport.Rmd", "html_document")
 library(kableExtra)
 render("2-CustSatReport-percents.Rmd", "html_document")
-
 render("2-CustSatReport-percents.Rmd", "word_document")
 
