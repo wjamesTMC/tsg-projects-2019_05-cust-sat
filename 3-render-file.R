a79e5b553d20b59055b6e8b694f4ad3afbcb5ff5
wjamesTMC <jamesw@csps.com>
2019-06-25 10:44:46 -0400

    Committing various changes

--- 3-render-file.R
+++ 3-render-file.R
@@ -1,2 +1,6 @@
 library(rmarkdown)
+library(kableExtra)
+render("2-CustSatReport-percents.Rmd", "html_document")
 render("2-CustSatReport.Rmd", "word_document")
+render("2-CustSatReport-percents.Rmd", "word_document")
+
