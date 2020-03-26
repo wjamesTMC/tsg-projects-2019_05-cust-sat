26b907887de7544c41db9f0295268dc861d0f69c
wjamesTMC <jamesw@csps.com>
2020-03-26 10:10:47 -0400

    Completed outliers comparison reports

--- 5-2020CustSat_ResponderGeneration.R
+++ 5-2020CustSat_ResponderGeneration.R
@@ -33,14 +33,10 @@ library(purrr)
 # File open, cleanup, and set up for the analysis
 #
 
-
 # Import and Open the data file / Establish the data set
-data_filename <- gs_title("2020 Survey All Users 20 02 05_T")
-data_filename <- gs_title("2020 Survey Combined Recipients")
+data_filename <- gs_title("2019-2020 TSG Satisfaction Survey")
 dat <- gs_read(data_filename, skip = 0, header = TRUE, stringsAsFactors = FALSE)
 dat <- as.data.frame(dat)
-dat[complete.cases(dat), ]
-# dat <- dat %>% select(Dept, Email, `First Name`, `Last Name`, 'Survey_date')
 
 # Consolidate Groups
 dat[dat == "Office of the Clerk"]  <- "Clerk"
@@ -128,33 +124,130 @@ dat[dat == "Office of the Treasurer"] <- "TRO"
 dat[dat == "Clerk's Office"] <- "Clerk"
 dat[dat == "Practitioner Activities"] <- "Clerk"
 
-dept_list <- unique(dat$Dept)
-dept_list <- sort(dept_list)
-num_depts <- length(dept_list)
-
 #-------------------------------------------------------------------------------
 #
 # Determine the departmental makeup of responders
 # 
 #-------------------------------------------------------------------------------
 
+# Open the list of responders from the various surveys
+
+data_filename <- gs_title("2020 Survey Combined Recipients")
+participant_dat <- gs_read(data_filename, skip = 0, header = TRUE, stringsAsFactors = FALSE)
+participant_dat <- as.data.frame(participant_dat)
+
+# Clean out NA's
+participant_dat[complete.cases(participant_dat), ]
+
+# Consolidate Groups
+participant_dat[participant_dat == "Office of the Clerk"]  <- "Clerk"
+participant_dat[participant_dat == "Clerks Office"]  <- "Clerk"
+participant_dat[participant_dat == "Technology Services Group"]  <- "TSG"
+participant_dat[participant_dat == "Brdcst & Multimedia Svs"] <- "BMPS"
+participant_dat[participant_dat == "Board Admin Office"] <- "Board Office"
+participant_dat[participant_dat == "ComPub Manager's Office"] <- "Committee Office"
+participant_dat[participant_dat == "Human Resources - SA"] <- "HR"
+participant_dat[participant_dat == "Monitor Editorial"] <- "CSM"
+participant_dat[participant_dat == "Bible Lesson Editorial & Products"]  <- "BLP"
+participant_dat[participant_dat == "Bible Lesson Editorial and Products"] <- "BLP"
+participant_dat[participant_dat == "Bible Lesson Products"] <- "BLP"
+participant_dat[participant_dat == "Mary Baker Eddy Library"] <- "MBEL"
+participant_dat[participant_dat == "Promotion & Design Services Group"] <- "Design Services"
+participant_dat[participant_dat == "Office of Records Mgmt"] <- "ORM"
+participant_dat[participant_dat == "Rights & Permissions Group"] <- "OGC"
+participant_dat[participant_dat == "Office of the Clerk"] <- "Clerk"
+participant_dat[participant_dat == "Clerks Office"] <- "Clerk"                                 
+participant_dat[participant_dat == "Board of Education"] <- "BOE"
+participant_dat[participant_dat == "Technology Services Group"] <- "TSG"                    
+participant_dat[participant_dat == "Monitor Editorial"] <- "CSM"
+participant_dat[participant_dat == "Bible Lesson Editorial & Products"] <- "BLP"              
+participant_dat[participant_dat == "Treasurer's Office"] <- "TRO"                             
+participant_dat[participant_dat == "Office of General Counsel"] <- "OGC"
+participant_dat[participant_dat == "General Publications"] <- "CSPS"
+participant_dat[participant_dat == "Board Admin Office"] <- "Board"                             
+participant_dat[participant_dat == "JSH Editorial"] <- "JSH"
+participant_dat[participant_dat == "Monitor Publishing"] <- "CSM"
+participant_dat[participant_dat == "Office of Records Mgmt"] <- "ORM"                         
+participant_dat[participant_dat == "Office of the Publishers Agent"] <- "OPA"
+participant_dat[participant_dat == "TMC Reading Room"] <- "ISD/TMC RR"                               
+participant_dat[participant_dat == "Bible Lesson Products"] <- "BLP"
+participant_dat[participant_dat == "Manager's Office"] <- "CSPS"                               
+participant_dat[participant_dat == "Church Activities"] <- "Church"
+participant_dat[participant_dat == "Nursing, Branch and Practitioner"] <- "Church"               
+participant_dat[participant_dat == "Facilities & Capital Proj"] <- "REPO"
+participant_dat[participant_dat == "Monitor Editorial Dept"] <- "CSM"                         
+participant_dat[participant_dat == "Committee on Publication"] <- "Committee Office"
+participant_dat[participant_dat == "Monitor"] <- "CSM"                                      
+participant_dat[participant_dat == "OCIO"] <- "TSG"
+participant_dat[participant_dat == "Practitioner Services"] <- "Church"                          
+participant_dat[participant_dat == "Christian Science Nursing Activities"] <- "Church"
+participant_dat[participant_dat == "Human Resources"] <- "HR"
+participant_dat[participant_dat == "Office of Language Svs"] <- "ISD/TMC RR"
+participant_dat[participant_dat == "The Mary Baker Eddy Library"] <- "MBEL"                    
+participant_dat[participant_dat == "Board of Lectureship"] <- "BOL"
+participant_dat[participant_dat == "ComPub Managers Office"] <- "Committee Office"
+participant_dat[participant_dat == "CSBD"] <- "Board"                                       
+participant_dat[participant_dat == "Office of the Publisher's Agent"] <- "OPA"
+participant_dat[participant_dat == "CSBD, Office of the Clerk"] <- "Clerk"                      
+participant_dat[participant_dat == "Board of Trustees"] <- "Board"
+participant_dat[participant_dat == "Customer Contact Center/Plaza Activities"] <- "CCC"       
+participant_dat[participant_dat == "JSH GP Manufacturing"] <- "CSPS"
+participant_dat[participant_dat == "CSM Jointly Managed"] <- "CSM"                            
+participant_dat[participant_dat == "Security"] <- "REPO"
+participant_dat[participant_dat == "Customer Contact Center/TMC Reading Rooms"] <- "CCC"     
+participant_dat[participant_dat == "JSHEditorial"] <- "JSH"
+participant_dat[participant_dat == "Real Estate Planning & Operations"] <- "REPO"              
+participant_dat[participant_dat == "Office of Language Services"] <- "ISD/TMC RR"
+participant_dat[participant_dat == "Practitioner Activities\\Plaza Activities"] <- "Church"      
+participant_dat[participant_dat == "Office of Records Mgmt/Mary Baker Eddy Library"] <- "MBEL"
+participant_dat[participant_dat == "CSM Marketing and Sales"] <- "CSM"                        
+participant_dat[participant_dat == "Digital Publishing Services"] <- "CSM"
+participant_dat[participant_dat == "CSPS Managers Office"] <- "CSPS"                           
+participant_dat[participant_dat == "Office of the General Counsel, Plaza Activities"] <- "OGC"
+participant_dat[participant_dat == "TSG"] <- "TSG"                                            
+participant_dat[participant_dat == "JSHEd Herald"] <- "JSH"
+participant_dat[participant_dat == "Site Svcs - InterOfficeServices"] <- "REPO"                
+participant_dat[participant_dat == "JSH Publishing"] <- "JSH"                                 
+participant_dat[participant_dat == "JSHQ Publishing"] <- "JSH"
+participant_dat[participant_dat == "ISD"] <- "ISD/TMC RR"                                            
+participant_dat[participant_dat == "JSH Production, Manufacturing"] <- "JSH"
+participant_dat[participant_dat == "ComPub"] <- "Committee Office"
+participant_dat[participant_dat == "Facilities &amp; Capital Proj"] <- "REPO"
+participant_dat[participant_dat == "Site Services"] <- "REPO"                                  
+participant_dat[participant_dat == "Monitor Editorial."] <- "CSM" 
+participant_dat[participant_dat == "Customer Contact Center"] <- "CCC"
+participant_dat[participant_dat == "Board Office"] <- "Board"
+participant_dat[participant_dat == "Treasurers Office"] <- "TRO"
+participant_dat[participant_dat == "Branch Activities"] <- "Church"
+participant_dat[participant_dat == "Contractor"] <- "TSG"
+participant_dat[participant_dat == "B&MPS / Clerk"] <- "BMPS"
+participant_dat[participant_dat == "Office of the Treasurer"] <- "TRO"
+participant_dat[participant_dat == "Clerk's Office"] <- "Clerk"
+participant_dat[participant_dat == "Practitioner Activities"] <- "Clerk"
+
+# Establiish the list of departments participating in the surveys
+dept_list <- unique(participant_dat$Dept)
+dept_list <- sort(dept_list)
+num_depts <- length(dept_list)
+
+# Build a dataframe to hold departmental participant comparisons
 dept_df  <- data.frame(Department = 1:num_depts * 2, count = 1:num_depts * 2,   s_date = 1:num_depts * 2)
 # count_3S20 = 1:num_depts, count_4S20 = 1:num_depts)
 
 j = 1
 for(i in 1:num_depts) {
-  x <- dat %>% filter(Dept == unique(dat$Dept)[i])
+  x <- participant_dat %>% filter(Dept == unique(participant_dat$Dept)[i])
   
   S1 <- x %>% filter(Survey_date == "1F19")
   S2 <- x %>% filter(Survey_date == "2W20")
   
-  dept_df[j, 1] <- unique(dat$Dept)[i]
+  dept_df[j, 1] <- unique(participant_dat$Dept)[i]
   dept_df[j, 2] <- nrow(S1)
   dept_df[j, 3] <- "1F19"
   
   j <- j + 1
   
-  dept_df[j, 1] <- unique(dat$Dept)[i]
+  dept_df[j, 1] <- unique(participant_dat$Dept)[i]
   dept_df[j, 2] <- nrow(S2)
   dept_df[j, 3] <- "2W20"
   
@@ -169,12 +262,13 @@ sort_df <- dept_df[order(rev(dept_df$Department)),]
 # Horizontal bar chart for result
 #
 
-ggplot(data = sort_df, aes(x = Department, y = count, fill = factor(s_date))) +
+ggplot(data = dept_df, aes(x = Department, y = count, fill = factor(s_date))) +
   geom_bar(position="dodge",stat="identity") +
   coord_flip() + 
   labs(title = "Number of Survey Invitations by Department by Quarter", subtitle = "Fall / Winter") +
   labs(y = "Count of Invitees") + 
-  guides(fill = guide_legend(reverse=TRUE))
+  scale_fill_brewer(palette="Blues") + ylab("Average Rating") +
+  theme_minimal() +guides(fill = guide_legend(reverse=TRUE))
 
 
 #-------------------------------------------------------------------------------
